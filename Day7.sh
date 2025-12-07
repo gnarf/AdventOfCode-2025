274607a01abc992faee2ce4201efad0325614e31
Mx. Corey Frang <gnarf37@gmail.com>
2025-12-07 14:57:45 -0500

    180

--- Day7.sh
+++ Day7.sh
@@ -1,4 +1,4 @@
 mapfile G;W=${#G};H=${#G[@]}
-for((Q=0;Q<H*W;Q++,x=Q%W));do ((o=x?o:0));g=${G[Q/W]:x:1}
-[ $g = S ]&& A[$x]=1;[ $g = ^ ]&&((A[x-1]+=A[x],A[x+1]+=A[x],A[x]=0))||((o+=A[x]))
+for((Q=0;Q<H*W;Q++,x=Q%W));do((o=x?o:0));g=${G[Q/W]:x:1}
+[ $g = S ]&&A[$x]=1;[ $g = ^ ]&&((A[x-1]+=A[x],A[x+1]+=A[x],A[x]=0))||((o+=A[x]))
 done;echo $o
